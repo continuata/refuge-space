b5414fbf7c03667171ac62437b0ba68787b8fe3b
Stu Kennedy <info@continuata.com>
2015-09-08 08:57:03 +0100

    fix local storage file

--- src/cljs/refuge_space/local-storage.cljs
+++ src/cljs/refuge_space/local-storage.cljs
@@ -1,16 +1,10 @@
 (ns refuge_space.local-storage)
 
-(defn set-item!
-  "Set `key' in browser's localStorage to `val`."
-  [key val]
+(defn set-item! [key val]
   (.setItem (.-localStorage js/window) key val))
 
-(defn get-item
-  "Returns value of `key' from browser's localStorage."
-  [key]
+(defn get-item [key]
   (.getItem (.-localStorage js/window) key))
 
-(defn remove-item!
-  "Remove the browser's localStorage value for the given `key`"
-  [key]
+(defn remove-item! [key]
   (.removeItem (.-localStorage js/window) key))
